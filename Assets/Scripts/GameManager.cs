52eed90568317f4c72c098033344a612b0dde4dc
branthnl <branthnl1@gmail.com>
2020-04-11 15:11:20 +0700

    Build test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public string[] lines;
+    public TextMeshProUGUI quoteText;
+
+    private void Start() {
+        lines = File.ReadAllLines(Application.dataPath + "/Texts/quotes.txt");
+        quoteText.text = Application.dataPath;//lines[0];
+    }
+
+    private void Update() {
+    }
+}
